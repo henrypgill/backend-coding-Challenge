1ea20e0ef9979db0061b85685911568959ba2ccd
henrypgill <hpgill312@gmail.com>
2024-01-18 22:38:33 +0000

    setup server to use nestjs instead of plain express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,8 @@
-import cors from "cors";
-import express from "express";
-import morgan from "morgan";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-const app = express()
-
-app.use(express.json())
-.use(cors())
-.use(morgan("tiny"))
-
-app.get("/", async (_req, res, next) => {
-    res.json({ msg: "Hello! There's nothing interesting for get/" });
-    next();
-  });
-
-
-
-  const port = 4000
-  app.listen(port, () => {
-    console.log(
-      `Server started listening for HTTP requests on port ${port}.  Let's go!`
-    );
-  });
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000);
+}
+bootstrap();

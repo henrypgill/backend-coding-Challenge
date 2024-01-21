dff82dbf8315f84a7b10d47cb964486cc7fd1e48
henrypgill <hpgill312@gmail.com>
2024-01-21 11:45:08 +0000

    created endpoint for posting an array of new books

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Book } from './types/book';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   welcome(): string {
     return this.appService.welcome();
+  }
+  @Post("/books/create")
+  async createBooks(@Body() body: {books: Book[]}): Promise<Book[] | Error>{
+      const createdBooks = await this.appService.createBooks(body.books);
+      return createdBooks
   }
 }

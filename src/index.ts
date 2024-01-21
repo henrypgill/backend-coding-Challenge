d796fc80b689066260d64b3d027e64c1dbd498c1
henrypgill <hpgill312@gmail.com>
2024-01-21 11:01:47 +0000

    created instance of mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupMongoClient } from './core/mongoSetup';
+
+
+const db_uri = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@database:27017`///${process.env.MONGODB_DATABASE}`
+export const database = setupMongoClient(db_uri)
+
+async function connectToDatabase() {
+  try {
+    console.log("connecting to database")
+    await database.client.connect()
+    console.log("successfully connected to database")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
 
 async function bootstrap() {
+
+  await connectToDatabase()
+
+
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log("App listening on port: " + port)
 }
-bootstrap();
+
+await bootstrap()

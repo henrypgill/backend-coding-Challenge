05912a15f9618b043c666b2f72fee6d50b36a929
henrypgill <hpgill312@gmail.com>
2024-01-21 11:45:29 +0000

    created service for adding new books to database

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { database } from '.';
+import type { Book } from './types/book';
 
 @Injectable()
 export class AppService {
   welcome(): string {
     return 'Welcome!';
+  }
+  async createBooks(books: Book[]): Promise<Book[]> {
+    const result = await database.books.insertMany(books)
+    // if (result.insertedCount !== books.length) {
+    //   throw new Error("Failed to create all books.")
+    // }
+    const createdBookIds = result.insertedIds
+    const booksCursor = await database.books.find(createdBookIds)
+    return booksCursor.toArray()
   }
 }

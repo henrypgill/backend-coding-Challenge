c502f99d213e3945098ededbaf945c9fe55c8099
henrypgill <hpgill312@gmail.com>
2024-01-21 11:46:28 +0000

    created service for retrieving books from database

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,6 +7,12 @@ export class AppService {
   welcome(): string {
     return 'Welcome!';
   }
+
+  async getBooks(): Promise<Book[]> {
+    const bookCursor = await database.books.find()
+    const books = bookCursor.toArray()
+    return books
+  }
   async createBooks(books: Book[]): Promise<Book[]> {
     const result = await database.books.insertMany(books)
     // if (result.insertedCount !== books.length) {
@@ -16,4 +22,5 @@ export class AppService {
     const booksCursor = await database.books.find(createdBookIds)
     return booksCursor.toArray()
   }
+
 }

95538ca42fdc74aadc017133754ccc88b462cae3
henrypgill <hpgill312@gmail.com>
2024-01-21 14:49:41 +0000

    removed redundant endpoints from app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
-import type { Book } from './types/book';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  welcome(): string {
-    return this.appService.welcome();
-  }
-
-  @Get("/books")
-  async getBooks(): Promise<Book[]>  {
-    return await this.appService.getBooks();
-  }
-
-  @Post("/books/create")
-  async createBooks(@Body() body: {books: Book[]}): Promise<Book[] | Error>{
-      const createdBooks = await this.appService.createBooks(body.books);
-      return createdBooks
-  }
-
+    @Get()
+    welcome(): string {
+        return this.appService.welcome();
+    }
 
 }

a5068b9fd451975a780503d6cd31c40732f8ae00
henrypgill <hpgill312@gmail.com>
2024-01-23 10:24:41 +0000

    added server startup logging and removed mongo uri

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,37 @@ import { AppModule } from "./app.module";
 import { setupMongoClient } from "./core/mongoSetup";
 import * as fs from "fs";
 
-const db_uri = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@database:27017`; ///${process.env.MONGODB_DATABASE}`
-export const database = setupMongoClient(db_uri);
-
 async function connectToDatabase() {
+    const db = setupMongoClient();
     try {
         console.log("connecting to database");
-        await database.client.connect();
+        await db.client.connect();
         console.log("successfully connected to database");
     } catch (error) {
         console.log(error);
     }
+    return db
 }
-console.log("starting up");
 async function bootstrap() {
-    const port = process.env.PORT || 4000;
-    console.log("Starting up NestJS server on port: " + port);
-    const app = await NestFactory.create(AppModule, {
-        snapshot: true,
-        abortOnError: false,
-    });
-    await app.listen(port);
-    console.log("App listening on port: " + port);
+    try {
+        const port = process.env.PORT || 4000;
+        console.log("Starting up NestJS server on port: " + port);
+        const app = await NestFactory.create(AppModule, {
+            snapshot: true,
+            abortOnError: false,
+        });
+        await app.listen(port);
+        console.log("App listening on port: " + port);
+    } catch (error) {
+        fs.writeFileSync("graph.json", PartialGraphHost.toString() ?? "");
+    console.error(error);
+    process.exit(1);
+    }
 }
 
-await connectToDatabase();
-
-bootstrap().catch((err) => {
-    fs.writeFileSync("graph.json", PartialGraphHost.toString() ?? "");
-    console.error(err);
-    process.exit(1);
-});
+console.time("Server startup time");
+console.log("Starting up server.");
+export const database = await connectToDatabase();
+await bootstrap();
+console.log("Server started up successfully.")
+console.timeEnd("Server startup time");

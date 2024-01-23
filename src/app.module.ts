add80641db204de9980213883a31ed1dcf951c08
henrypgill <hpgill312@gmail.com>
2024-01-23 10:13:36 +0000

    removed redundant app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module, type MiddlewareConsumer, type ModuleMetadata } from '@nestjs/common';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
 import { AppLoggerMiddleware } from './core/AppLogger';
-import { BooksModule } from './modules/books/books.module';
 import { AuthorsModule } from './modules/authors/authors.module';
-import { PublishersModule } from './modules/publishers/publishers.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BooksModule } from './modules/books/books.module';
 import { DummyDataModule } from './modules/dummyData/dummyData.module';
+import { PublishersModule } from './modules/publishers/publishers.module';
 
 const imports: ModuleMetadata["imports"] = [
     BooksModule,
@@ -23,8 +21,6 @@ process.env.NODE_ENV !== 'production' && imports.push(...[
 
 @Module({
     imports: imports,
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {
     configure(consumer: MiddlewareConsumer): void {
